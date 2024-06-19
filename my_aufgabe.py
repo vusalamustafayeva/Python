5d3b5b6c70c53094ea2673533f4355322bbc2c42
Vusala <148570431+vusalamustafayeva@users.noreply.github.com>
2024-06-19 14:10:11 +0200

    Update my_aufgabe.py
    stadt_eingabe.lower() und info_typ_eingabe.lower() wurden verwendet, 
    um die Nutzereingaben in Kleinbuchstaben umzuwandeln.
    Durch die Verwendung der Funktionen capitalize() und 
    title() wurde sichergestellt, dass die Ausgaben ordentlicher aussehen.

--- my_aufgabe.py
+++ my_aufgabe.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import pytz
 
+#Hauptprogram
 def lokale_zeit_erhalten(stadtname, info_typ):
     stadt_zeitzonen = {
         'san fransisko': 'America/Los_Angeles',
@@ -22,21 +23,21 @@ def lokale_zeit_erhalten(stadtname, info_typ):
         if info_typ == 'zeit':
             return lokale_zeit.strftime('%H:%M:%S')
         elif info_typ == 'datum':
-            return lokale_zeit.strftime('%d-%m-%Y')
+            return lokale_zeit.strftime('%d.%m.%Y')
     else:
         return "Der eingegebene Stadtname ist ungültig."
 
+ #Hauptschleife
 while True:
     stadt_eingabe = input("Bitte geben Sie den Namen der Stadt ein, deren lokale Zeit oder Datum Sie wissen möchten: ")
     if stadt_eingabe.lower() == 'ende':
-        print("Ups, es ist beendet:).")
+        print("Ups, es ist beendet:). ")
         break
     info_typ_eingabe = input("Möchten Sie die Zeit oder das Datum wissen? (zeit/datum): ")
     if info_typ_eingabe.lower() not in ['zeit', 'datum']:
         print("Ungültige Eingabe. Bitte geben Sie 'zeit' oder 'datum' ein.")
         continue
-    print(f"{info_typ_eingabe.title()} für {stadt_eingabe.title()}: {lokale_zeit_erhalten(stadt_eingabe, info_typ_eingabe)}")
-
+    print(f"{info_typ_eingabe.capitalize()} für {stadt_eingabe.title()}: {lokale_zeit_erhalten(stadt_eingabe, info_typ_eingabe.lower())}")
 
 
 
@@ -46,7 +47,7 @@ die lokale Zeit für verschiedene Städte weltweit zu berechnen.
 
 Bibliotheken: datetime, pytz
 
-Funktion: lokale_zeit_erhalten - '%Y-%m-%d %H:%M:%S'
+Funktion:(Hauptprogram) lokale_zeit_erhalten - '%d-%m-%Y' '%H:%M:%S'
 
 Um das Programm laufen zu lassen, ohne es anzuhalten, 
 wurde eine While-Schleife geschrieben.
@@ -54,5 +55,10 @@ wurde eine While-Schleife geschrieben.
 Dieser Code fragt den Benutzer nach Eingabe des Ortsnamens, 
 ob er "zeit" oder "datum" wünscht, und gibt die Informationen entsprechend zurück.
 Der Benutzer kann die Schleife beenden, indem er "ende" eingibt. 
+
+stadt_eingabe.lower() und info_typ_eingabe.lower() wurden verwendet, 
+um die Nutzereingaben in Kleinbuchstaben umzuwandeln.
+Durch die Verwendung der Funktionen capitalize() und 
+title() wurde sichergestellt, dass die Ausgaben ordentlicher aussehen.
 
 """
